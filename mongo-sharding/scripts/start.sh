ba36369b8f5e31240f474a74347d410eda2d88fe
dmitrydi <d.dmitrook@gmail.com>
2024-11-26 23:12:52 +0300

    sprint-2: made tasks 2,3
    task 4 wrong
    
    task 4 fixed
    
    added readme, diagram
    
    fixed readme
    
    update readme
    
    fixed reference
    
    fixed ref in dir
    
    fixed scripts

---
+++ mongo-sharding/scripts/start.sh
@@ -0,0 +1,56 @@
+# config server
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+)
+EOF
+
+sleep 5s
+
+# shard 1
+docker compose exec -T shard1_1 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+)
+EOF
+
+sleep 5s
+
+# shard 2
+docker compose exec -T shard2_1 mongosh --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard2:27019" },
+      ]
+    }
+)
+EOF
+
+sleep 5s
+
+# router
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/shard1:27018")
+sh.addShard( "shard2/shard2:27019")
+sh.enableSharding("somedb")
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+EOF
+
+
+
+
